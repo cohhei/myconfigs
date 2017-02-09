b3e712c1c96317fe8da207560d2327e965a269c3
Kimura <kimura@MBP.local>
2017-02-09 10:46:48 +0900

    fix

--- home/set.sh
+++ home/set.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -Ceu
-for file in `\find . -maxdepth 1 -type f`; do
-  ln $file ~/
+for file in `\find . -maxdepth 1 -type f ! -name '*.sh'`; do
+  ln -f $file ~/
   echo $file
 done
 git config --global core.excludesfile ~/.gitignore_global
